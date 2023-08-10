00040bba58195f9dcf44636c4b47ede9eedf6b54
João Darwin <joaodarwin.ip22@gmail.com>
2023-08-09 21:59:01 -0300

    feat: Criar page about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import imgAboutUs from "../../assets/About us page-amico.png";
+import imgOurMission from "../../assets/Rocket-amico.png";
+import "./About.css"
 
 const About = () => {
     return (
-        <div>
-            <h1>Sobre nós</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione, cupiditate voluptas consequuntur rem quidem in suscipit laudantium necessitatibus dignissimos tempora sunt perferendis quo repellat dolorum quisquam quasi obcaecati ab ut!</p>
+        <div className='container containerAbout'>
+            <div className='div1'>
+                <h1 className='titulo'>Nossa história</h1>
+                <div className='divImgParagrafo'>
+                    <p className='paragrafo paragrafoAbout' style={{ float: "left" }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, expedita unde, quidem at rem consectetur sequi qui, laboriosam eum suscipit nihil iste! Hic delectus unde earum est totam, sunt cum!Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, expedita unde, quidem at rem consectetur sequi qui, laboriosam eum suscipit nihil iste! Hic delectus unde earum est totam, sunt cum</p>
+                    <img style={{ float: "right" }} className='imgPageInicial' src={imgAboutUs} alt="Imagem nossa história" />
+                </div>
+            </div>
+            <div className='div2'>
+                <h1 className='titulo'>Nossa missão</h1>
+                <div className='divImgParagrafo'>
+                    <img style={{ float: "left" }} className='imgPageInicial' src={imgOurMission} alt="Imagem nossa missão" />
+                    <p className='paragrafo paragrafoAbout' style={{ float: "right" }}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda minima minus alias excepturi et, ipsum cumque voluptas velit dignissimos, perferendis voluptatem mollitia libero repudiandae, quia eum quo accusantium deserunt. Voluptate?Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, expedita unde, quidem at rem consectetur sequi qui, laboriosam eum suscipit nihil iste! Hic delectus unde earum est totam, sunt cum</p>
+                </div>
+            </div>
         </div>
     )
 }
